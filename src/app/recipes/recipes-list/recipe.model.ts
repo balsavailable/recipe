d5af51e64e8d956772fe0cd05ca616afb9427980
Balakrishnan <balsavailable@gmail.com>
2018-10-07 16:27:57 +0530

    "made changes on list of recipes"

---
+++ src/app/recipes/recipes-list/recipe.model.ts
@@ -0,0 +1,67 @@
+
+export class Recipe {
+
+    private _name: string;
+    private _description: string;
+    private _image: string;
+
+
+
+    constructor(name: string, description: string, image: string) {
+        this._name = name;
+        this._description = description;
+        this._image = image;
+    }
+
+
+    /**
+     * Getter name
+     * @return {string}
+     */
+    public get name(): string {
+        return this._name;
+    }
+
+    /**
+     * Getter description
+     * @return {string}
+     */
+    public get description(): string {
+        return this._description;
+    }
+
+    /**
+     * Getter image
+     * @return {string}
+     */
+    public get image(): string {
+        return this._image;
+    }
+
+    /**
+     * Setter name
+     * @param {string} value
+     */
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    /**
+     * Setter description
+     * @param {string} value
+     */
+    public set description(value: string) {
+        this._description = value;
+    }
+
+    /**
+     * Setter image
+     * @param {string} value
+     */
+    public set image(value: string) {
+        this._image = value;
+    }
+
+}
+
+
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [new Recipe("French Fries ", "Tomator ", "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Fries_2.jpg/800px-Fries_2.jpg"),
+  new Recipe("Veg Meals", "meals with variety of vegitables (unlimited)", "https://www.mangoo.in/wp-content/uploads/2018/01/VEG-MEALS.jpg"),
+  new Recipe("peanut", "very hot tasty spice peanuts", "https://1.bp.blogspot.com/-HwmhiZFz57U/WqbFhlRaemI/AAAAAAAAR9o/vyWVbiM992UhOaaw9GRuzjMr8V0Iq9UPQCLcBGAs/s320/26171050_1938697659729168_8721613958306216658_o.jpg")];
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
